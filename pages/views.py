c61cda967c9785710bbca8ba13194ac757c9ba59
Sandip Bhagat <sandipbgt@gmail.com>
2017-01-20 08:35:46 +0545

    Redirect to dashboard page when user is logged in

--- pages/views.py
+++ pages/views.py
@@ -4,13 +4,15 @@ from django.utils import timezone
 from django.contrib.auth.decorators import login_required
 from django.db.models import Sum
 from django.db.models.functions import TruncMonth
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def home(request):
     """
     Home page
     """
+    if request.user.is_authenticated():
+        return redirect('dashboard')
     context = {}
     return render(request, 'pages/home.html', context)
 
