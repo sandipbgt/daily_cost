a4ff1e8997b972a3e822c5f88ee6f385392f9f9f
Sandip Bhagat <sandipbgt@gmail.com>
2017-02-01 23:45:01 +0545

    Added feature to search by category name

--- transactions/filters.py
+++ transactions/filters.py
@@ -1,12 +1,23 @@
+from functools import reduce
+
 import django_filters
+from django.db.models import Q
 
 from .models import Transaction
 
 
 class TransactionFilter(django_filters.FilterSet):
-    search = django_filters.CharFilter(name='title', lookup_expr='icontains')
+    search = django_filters.CharFilter(name='title', method='search_transaction')
     from_date = django_filters.DateFilter(name='transaction_date', lookup_expr='gte')
     to_date = django_filters.DateFilter(name='transaction_date', lookup_expr='lte')
+
+    def search_transaction(self, queryset, name, value):
+        values = value.split()
+        return queryset\
+            .filter(
+                reduce(Q.__or__, [Q(title__icontains=query) for query in values]) |
+                reduce(Q.__or__, [Q(category__name__icontains=query) for query in values])
+            ).distinct()
 
     class Meta:
         model = Transaction
